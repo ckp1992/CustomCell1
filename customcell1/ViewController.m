de21fa7609f75d95611bf6baf4b659c8f0af75f3
 陈鲲鹏 <674027158@qq.com>
2014-10-11 16:41:15 +0800

    增加Plist和图片的读取

--- customcell1/ViewController.m
+++ customcell1/ViewController.m
@@ -21,6 +21,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //加载PLIST和图片
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *plistURL = [bundle URLForResource:@"friends" withExtension:@"plist"];
+    NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfURL:plistURL];
+    
+    NSMutableArray *tmpDataArray = [[NSMutableArray alloc] init];
+    NSMutableArray *tmpImageArray = [[NSMutableArray alloc] init];
+    
+    //循环加载
+    
+    for(int i = 0;i<[dictionary count];i++){
+        //加载plist内容
+        NSString *key = [[NSString alloc] initWithFormat:@"%i",i+1];
+        NSDictionary *tmpDic = [dictionary objectForKey:key];
+        [tmpDataArray addObject:tmpDic];
+        
+        //加载图片内容
+        NSString *strImageUrl = [[NSString alloc] initWithFormat:@"%i.jpg",i+1];
+        UIImage *image = [UIImage imageNamed:strImageUrl];
+        [tmpImageArray addObject:image];
+    }
+    
+    self.dataList = [tmpDataArray copy];
+    self.imageList = [tmpImageArray copy];
 }
 
 - (void)didReceiveMemoryWarning {
