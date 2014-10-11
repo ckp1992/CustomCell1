02bf4bed8e65d455ecba87e47384bb575385affc
 陈鲲鹏 <674027158@qq.com>
2014-10-11 15:25:59 +0800

    创建cellxib文件

---
+++ customcell1/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  customcell1
+//
+//  Created by  陈鲲鹏 on 14-10-11.
+//  Copyright (c) 2014年  陈鲲鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ customcell1/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  customcell1
+//
+//  Created by  陈鲲鹏 on 14-10-11.
+//  Copyright (c) 2014年  陈鲲鹏. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
